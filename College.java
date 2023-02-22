9e895a06d8eac2a3dbf2bc39f121c8dc6c9486cc
AbdulwajidAW <123639800+AbdulwajidAW@users.noreply.github.com>
2023-02-22 17:23:13 +0530

    org

---
+++ College.java
@@ -0,0 +1,14 @@
+package org.system;
+
+public class College {
+	public void collageName(String name) {
+		System.out.println("Collage Name :"+name);
+	}
+	public void collageCode(int code) {
+		System.out.println("Collage Code :"+code);
+	}
+	public void collageRank(String rank) {
+		System.out.println("Collage Rank :"+rank);
+
+	}
+}
